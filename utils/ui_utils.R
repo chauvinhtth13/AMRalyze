9e866741956714f352b12eada0ce059b69001cc1
CHAU VINH <chauvinhtth13@gmail.com>
2025-04-04 17:03:48 +0700

    Update ui_utils.R

--- utils/ui_utils.R
+++ utils/ui_utils.R
@@ -1,6 +1,6 @@
 detect_column <- function(headers, patterns) {
   headers <- tolower(headers)
-  patterns <- tolower(patterns)
+  patterns <- paste0("\\b",tolower(patterns),"\\b")
   headers <- gsub("_", " ", headers)
   matches <- grep(paste(patterns, collapse = "|"), headers, ignore.case = FALSE)
   if (length(matches) > 0) headers[matches[1]] else "None"
